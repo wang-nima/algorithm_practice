25cef884829f06d01d55f24918d10f6a7f6ca636
shawn <jak47042007@163.com>
2015-02-26 12:14:38 -0800

    mergeSort written by my self

---
+++ mergeSort.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+using namespace std;
+
+void combine(int *a, int n1, int n2) {
+	int i = 0;
+	int j = n1;
+	int k = 0;
+	int *b = (int*) malloc( (n1 + n2) * sizeof(int) );
+
+	while(i < n1 && j < n1 + n2) {
+		if(a[i] < a[j]) {
+			b[k] = a[i];
+			i++;
+		} else {
+			b[k] = a[j];
+			j++;
+		}
+		k++;
+	}
+	//combine the rest part to b
+	if(i == n1) {
+		while(j != n1 + n2) {
+			b[k] = a[j];
+			j++;
+			k++;
+		}
+	} else {	//j == n2
+		while(i != n1) {
+			b[k] = a[i];
+			i++;
+			k++;
+		}
+	}
+	//k == n1 + n2 now
+	memcpy(a, b, (n1+n2)*sizeof(int));
+}
+
+void mergeSort(int *a, int n) {
+	if(n == 1)	return;
+	mergeSort(a, n / 2);
+	mergeSort(a + n / 2, n - n / 2);		//the size of second part can be odd and can be larger that the first part by 1
+	combine(a, n / 2, n - n / 2);
+}
+
+void print(int *a, int n) {
+	int i;
+	for(i = 0; i < n; i++) {
+		cout << a[i] << " ";
+	}
+	cout << endl;
+}
+
+int main ()
+{
+	int a[] = {5,2,8,9,0,10,3,4,1};
+	mergeSort(a,9);
+	print(a,9);
+	return 0;
+}
